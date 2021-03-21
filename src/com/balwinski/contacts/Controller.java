bd1e0a1464595f1abbde1f8a79ec2074c4726014
Jerzy Balwinski <jurek_b@wp.pl>
2021-03-21 12:52:36 +0100

    TableView that shows values, test data, XML save engine implemented

--- src/com/balwinski/contacts/Controller.java
+++ src/com/balwinski/contacts/Controller.java
@@ -1,4 +1,16 @@
 package com.balwinski.contacts;
 
+import com.balwinski.contacts.datamodel.Contact;
+import com.balwinski.contacts.datamodel.ContactData;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
 public class Controller {
+
+    @FXML
+    private TableView<Contact> tableView;
+
+    public void initialize() {
+        tableView.setItems(ContactData.getInstance().getContacts());
+    }
 }
--- src/com/balwinski/contacts/Main.java
+++ src/com/balwinski/contacts/Main.java
@@ -1,12 +1,21 @@
 package com.balwinski.contacts;
 
+import com.balwinski.contacts.datamodel.ContactData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    @Override
+    public void init() throws Exception {
+        ContactData.getInstance().loadContacts();
+//        ContactData.getInstance().loadTestContacts();
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -14,6 +23,11 @@ public class Main extends Application {
         primaryStage.setTitle("JB's simple Contacts App");
         primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        ContactData.getInstance().saveContacts();
     }
 
 
